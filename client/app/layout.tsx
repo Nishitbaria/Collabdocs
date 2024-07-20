6194df3f0f32451040cb87d9f70e773cda3d9366
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-07-20 14:28:31 +0000

    chore: Update npm dependencies and add Clerk themes

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,9 +1,10 @@
-import "@/styles/globals.css"
 import { Inter as FontSans } from "next/font/google"
+import './globals.css'
 
 import { cn } from "@/lib/utils"
 import { Metadata } from "next";
-
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes"
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -19,15 +20,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
+
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: {
+          colorPrimary: "#3371FF",
+          fontSize: '16px'
+        },
+      }}
+    >
+      <html lang="en">
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
